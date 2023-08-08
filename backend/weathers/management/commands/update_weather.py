3d6d3e7d601f08054937677422cd1d123b5e476d
BORB <qzsec299@naver.com>
2023-08-08 20:42:35 +0900

    Refactor : celery 삭제

--- backend/weathers/management/commands/update_weather.py
+++ backend/weathers/management/commands/update_weather.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 from django.core.management.base import BaseCommand
 from weathers import models
-from celery import Celery
+
 
 NAME = "weathers"
 
@@ -55,42 +55,3 @@ class Command(BaseCommand):
         except Exception as e:
             print(e)
             self.stdout.write(self.style.ERROR("Error updating weather data."))
-
-
-# @Celery.task(name="update_weather")
-# def update_weather_task():
-#     try:
-#         # Fetch data from the API
-#         response = requests.get(
-#             f'{API_URL}?apikey={env("ACCU_WEATHER_API_KEY")}&language={LANGUAGE}'
-#         )
-#         data = response.json()
-
-#         # Update the database with the fetched data
-#         for item in data:
-#             key = item["Key"]
-#             country_code = item["Country"]["ID"]
-#             country_localized_name = item["Country"]["LocalizedName"]
-#             localized_name = item["LocalizedName"]
-#             temperature_value = item["Temperature"]["Metric"]["Value"]
-#             latitude = item["GeoPosition"]["Latitude"]
-#             longitude = item["GeoPosition"]["Longitude"]
-
-#             models.Weather.objects.update_or_create(
-#                 key=key,
-#                 defaults={
-#                     "country_code": country_code,
-#                     "country_localized_name": country_localized_name,
-#                     "localized_name": localized_name,
-#                     "temperature_value": temperature_value,
-#                     "latitude": latitude,
-#                     "longitude": longitude,
-#                 },
-#             )
-
-#     except Exception as e:
-#         print(e)
-
-
-# def handle(self, *args, **options):
-#     update_weather_task.delay()
