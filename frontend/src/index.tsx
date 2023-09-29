e3dba1ff3bdfe909ea129fe68f14fe5ed2b0b51f
BORB <qzsec299@naver.com>
2023-09-29 23:02:06 +0900

    Feat: PWA 설정.
    서비스 워커 추가.
    매니페스트 수정.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import "@fontsource/black-han-sans";
 import "@fontsource/fira-sans";
 import "@fontsource/noto-sans-kr";
 import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -22,3 +23,8 @@ root.render(
 );
 
 // App은 Router로 교체 됨
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
